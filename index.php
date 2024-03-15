149a393a49e51900013686da713d8acca76aad05
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-03-15 14:58:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+   <form action="">
+    <label for="">Inserisci una parola</label>
+    <input type="text">
+    <input type="submit">
+   </form>
+</body>
+</html>
